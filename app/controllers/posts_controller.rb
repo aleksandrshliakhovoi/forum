140a19424a7e976241fa7e5bf9a05a14763cc4b1
aleksandrshliakhovoi <aleksandrshliakhovoi@gmail.com>
2021-03-02 21:50:36 +0200

    Add update settings: temlate, controller, rout

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,15 @@
 class PostsController < ApplicationController
   def index
-    @post = Post.all()
+    @posts = Post.all()
   end
 
   def show
     @post = Post.find(params[:id]) #id from request_path which is /post/:id
+  end
+
+  def edit
+    @post = Post.find(params[:id]) #id from request_path which is /post/:id
+    @topics = Topic.all() #get all topics from db
   end
 
   def new
@@ -12,10 +17,21 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(params.require(:post).permit(:title, :body, :topic_id)) #nested params
+    @post = Post.new(post_params) #nested params
     @post.save
 
     redirect_to '/posts/' + @post.id.to_s
   end
   
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params) 
+    redirect_to '/posts/' + @post.id.to_s
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :topic_id)
+  end
 end
