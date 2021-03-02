3899927303d9f1a55b929a8a5833525cfe67561a
aleksandrshliakhovoi <aleksandrshliakhovoi@gmail.com>
2021-03-02 21:21:44 +0200

    Update erb templates

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,21 @@
 class PostsController < ApplicationController
   def index
+    @post = Post.all()
   end
 
   def show
+    @post = Post.find(params[:id]) #id from request_path which is /post/:id
   end
 
-  def def new
+  def new
     @topics = Topic.all() #get all topics from db
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :body, :topic_id)) #nested params
+    @post.save
+
+    redirect_to '/posts/' + @post.id.to_s
   end
   
 end
